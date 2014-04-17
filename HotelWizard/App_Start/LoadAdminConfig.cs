f4f728dc9f1d8d90314246eafaabeb926afabf5b
jenny cooper <jennycooper@eircom.net>
2014-04-17 22:08:22 +0100

    remove drag n drop, add event to select button, add new module for edit pic

--- HotelWizard/App_Start/LoadAdminConfig.cs
+++ HotelWizard/App_Start/LoadAdminConfig.cs
@@ -11,13 +11,10 @@ namespace HotelWizard.App_Start
     public class LoadAdminConfig
     {      
         public static void loadData(){
-            //call a method to get the admin configuration details from the database
-           
+                       
             //create a config singleton instance
            ConfigSingleton config = ConfigSingleton.Instance;
-            //assign attributers of the singleton instance from the values
-            //that have been retrieved from the database
-           
+                  
            System.Diagnostics.Debug.WriteLine(config.numRooms);
            System.Diagnostics.Debug.WriteLine(config.numTables);
         }           
