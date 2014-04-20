c08f5971a44f3722f0b75cf10c78d149ec030979
Lucjan Stepien <lucjan.stepien@student.ncirl.ie>
2014-04-20 20:50:23 +0100

    creating models for restaurant

---
+++ HotelWizard/Models/RestaurantBooking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelWizard.Models
+{
+    public class RestaurantBooking
+    {
+        public int RestaurantBookingId { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime BookingDate { get; set; }
+        public TimeSpan BookingTime { get; set; }
+        public int NumOfPeople { get; set; }
+        public int customerID { get; set; }
+        public virtual RestaurantCustomer customer { get; set; }
+    }
+}
---
+++ HotelWizard/Models/RestaurantCustomer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelWizard.Models
+{
+    public class RestaurantCustomer
+    {
+        public int ID { get; set; }
+        public String FirstName { get; set; }
+        public string SecondName { get; set; }
+        public String Email { get; set; }
+        public String Phone { get; set; }
+        public virtual ICollection<RestaurantBooking> Bookings { get; set; }
+
+        //method to find customers by customer name
+       // public static List<RestaurantCustomer> findByName(String name)
+       // {
+           // ApplicationDbContext db = new ApplicationDbContext();
+           // string query = "SELECT * FROM RestaurantCustomers WHERE SecondName = @p0";
+           // System.Data.Entity.Infrastructure.DbRawSqlQuery<RestaurantCustomer> data = db.RestaurantCustomers.SqlQuery(query, name);
+          //  List<RestaurantCustomer> customers = data.ToList();
+           // return customers;
+        //}
+    }
+}
